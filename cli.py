f36032da8303c4bd72f0f30037d6c36ec59c7abf
monjiapawne <iambrendamore@gmail.com>
2025-07-03 23:10:23 -0400

    parsed traceroute for linux into list with latency and ips

--- cli.py
+++ cli.py
@@ -5,8 +5,5 @@ def main():
     target = sys.argv[1]
     hops = traceroute.traceroute(target)
 
-    print("Traceroute Results:")
-    print(hops)
-
 if __name__ == "__main__":
     main()
--- core/traceroute.py
+++ core/traceroute.py
@@ -22,7 +22,26 @@ def parse_traceroute(output):
     Args:
         output (str): raw traceroute output
     """
-    return output 
+    results = []
+    if OS_NAME == 'Windows':
+        print("Windows")
+    if OS_NAME == 'Linux':
+        for hops in output.splitlines()[1:]:
+            if '*' in hops:
+                results.append(('*', None))  # for null reponses
+            elif '(' in hops and ')' in hops:
+                # parse ip
+                hop_ip = hops.split('(')[1].split(')')[0]
+                # parse latency
+                parts = hops.split()
+                for i, p in enumerate(parts):
+                    if p.endswith('ms'):
+                        latency = float(parts[i - 1])
+                        results.append((hop_ip, latency))
+                        break
+        print(results)
+        return results
+    return ""
 
 def traceroute(host):
     output = run_traceroute(host)
