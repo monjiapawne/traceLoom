9483651efe43631774d8b397db5fa4a4575db36e
ajsirag <ajsirag@gmail.com>
2025-07-10 20:36:20 -0500

    Fixed enrich.py scan ports function

--- core/enrich.py
+++ core/enrich.py
@@ -3,7 +3,7 @@ import psutil
 
 from node import Node
 
-ip_list = [('192.168.1.254', 8.749), ('107.215.132.1', 8.871), ('71.151.37.28', 8.407), ('*', None), ('*', None), ('66.198.27.4', 11.646), ('63.243.129.56', 20.143), ('66.110.48.14', 19.972), ('*', None), ('*', None), ('*', None), ('*', None), ('*', None), ('*', None), ('*', None), ('143.244.220.150', 30.133)]
+ip_list = [('192.168.1.254', 14.553), ('107.215.132.1', 12.96), ('71.151.37.28', 11.504), ('64.125.25.42', 34.358), ('64.125.41.223', 36.35), ('64.74.203.34', 37.632), ('170.249.244.66', 36.41), ('108.160.145.12', 36.982)]
 node_list = []
 
 def create_node_list(ip_list):
@@ -15,7 +15,6 @@ def create_node_list(ip_list):
         List(Node): List of Nodes with IP field filled
     """
     for ip, latency in ip_list:
-        print(ip, latency)
         node_list.append(Node(ip=ip, latency=latency))
     return node_list
 
@@ -30,11 +29,9 @@ def find_mac_address(node_list):
         target_hardware_address = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
 
         broadcast_packet = target_hardware_address/target_ip_address
-        ans, unans = scapy.srp(broadcast_packet, timeout=0.01, verbose=0)
+        ans, unans = scapy.srp(broadcast_packet, timeout=0.1, verbose=0)
 
-        if unans:
-            print("None")
-        else:
+        if ans:
             src_mac_address = (ans[0][1]).src
             node.mac_address = src_mac_address
     
@@ -47,19 +44,19 @@ def scan_ports(node_list):
     """
     PORTS = {
         20 :    'FTP', 
-        21:     "SFTP",
+        21:     'SFTP',
         22:     "SSH",
-        23:     "Telnet",
-        25:     "SMTP",
-        53:     "DNS",
-        80:     "HTTP",
-        110:    "POP3",
-        111:    "rpcbind",
-        443:    "HTTPS"
+        23:     'Telnet',
+        25:     'SMTP',
+        53:     'DNS',
+        80:     'HTTP',
+        110:    'POP3',
+        111:    'rpcbind',
+        443:    'HTTPS'
     }
 
     for node in node_list:
-        if node.ip != "*":
+        if node.ip != '*':
             replies = {}
             destination_ip = node.ip
             nics = psutil.net_if_addrs()
@@ -71,14 +68,16 @@ def scan_ports(node_list):
             ip_layer = scapy.IP(src=my_ip, dst=destination_ip)
             for port in PORTS:
                 tcp_layer = scapy.TCP(sport=12345, dport=port, seq=1000)
-                response = scapy.sr1(ip_layer/tcp_layer, timeout=0.01, verbose=0)
+                response = scapy.sr1(ip_layer/tcp_layer, timeout=0.1, verbose=0)
 
                 if response is None:
                     replies.update({port : 'filtered'})
-                elif response['TCP'].flags == 'SA':
-                    replies.update({port : 'open'})
                 else:
-                    replies.update({port : 'closed'})
+                    if response['IP'].proto == 6:
+                        if response['TCP'].flags == 'SA':
+                            replies.update({port : 'open'})
+                        else:
+                            replies.update({port : 'closed'})
             
             node.ports = replies
 
