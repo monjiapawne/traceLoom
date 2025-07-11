3b540b5fd084bfaff86f7e0a1558996fcbd47460
monjiapawne <iambrendamore@gmail.com>
2025-07-11 01:21:59 -0400

    feat: flags for enriching, custom linux traceroute combined with windows to avoid needing sudo for icmp traceroute

--- cli.py
+++ cli.py
@@ -8,10 +8,16 @@ def parse_args():
         formatter_class=argparse.RawTextHelpFormatter,
         epilog="Example: traceLoom -t 8.8.8.8 -l info"
     )
-    parser.add_argument('-t', '--target', type=str, 
-                        required=True)
+    parser.add_argument('-t', '--target', type=str, required=True,
+                        help='target hostname or ip address')
     parser.add_argument('-l', '--logging', type=str.lower, 
-                        choices=['info', 'warning'], default='INFO')
+                        choices=['info', 'warning'], default='WARNING',
+                        help="choose logging level")
+    
+    parser.add_argument('--ports', action='store_true',
+                        help='toggle port scannning')
+    parser.add_argument('--mac', action='store_true',
+                        help='toggle mac scanning')
 
     return parser.parse_args()
 
@@ -19,8 +25,12 @@ def main(args):
     logging.basicConfig(level=getattr(logging, (args.logging).upper()), format='[%(levelname)s] %(message)s')
     hops = traceroute.traceroute(args.target)
     node_list = enrich.create_node_list(hops)
-    node_list = enrich.find_mac_address(node_list)
-    node_list = enrich.scan_ports(node_list)
+
+    if args.ports:
+        node_list = enrich.scan_ports(node_list)
+    if args.mac:
+        node_list = enrich.find_mac_address(node_list)
+
     for node in node_list:
         print(node)
     
--- core/traceroute.py
+++ core/traceroute.py
@@ -5,23 +5,32 @@ from typing import List, Tuple, Optional
 import socket
 import ipaddress
 import logging
+import re
 
 # Constants
 OS_NAME = platform.system()
-MAX_HOPS=30
+MAX_HOPS = 30
+PING_TIMEOUT="0.05"  # seconds eg: 0.04 s = 40 ms
 
-def _stantize_host(host):
+def _sanitize_host(host):
     try:
         ipaddress.IPv4Address(host)
         return host
     except ValueError:
         return socket.gethostbyname(host)
 
-def _run_traceroute_windows(host):
+def _run_traceroute(host):
     results = []
     ttl = 1
     while ttl <= MAX_HOPS:
-        cmd = ['ping', '-n', '1', '-i', str(ttl), '-w', '200', host]  # ping -n 1 -i 2 -w 200 8.8.8.8
+
+        if OS_NAME == 'Windows':
+            cmd = ['ping', '-n', '1', '-i', str(ttl), '-w', str(PING_TIMEOUT * 1000), host]
+        elif OS_NAME == 'Linux':
+            cmd = ['ping', '-c', '1', '-t', str(ttl), '-W', str(PING_TIMEOUT), host]
+        else:
+            raise NotImplementedError(f"Unsupported OS: {OS_NAME}")
+        
         start = time.time()
         result = subprocess.run(cmd, capture_output=True, text=True)
         end = time.time()
@@ -29,8 +38,14 @@ def _run_traceroute_windows(host):
 
         try:
             line = next(l for l in result.stdout.splitlines() if "from" in l.lower())
-            hop_ip = line.split("from ")[1].split(":")[0]
+            if OS_NAME == 'Windows':
+                hop_ip = line.split("from ")[1].split(":")[0]
+            else:
+                match = re.search(r'(?i)from ([\d\.]+)', line)
+                hop_ip = match.group(1) if match else "*"
+
             results.append((hop_ip, latency))
+
             if hop_ip == host:
                 return results
 
@@ -39,39 +54,17 @@ def _run_traceroute_windows(host):
 
         ttl += 1
 
-def _run_traceroute_unix(host):
-    cmd = ["traceroute", "-q", "1",  host]
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    output = result.stdout
-
-    results = []
-    for hops in output.splitlines()[1:]:
-        if '*' in hops:
-            results.append(('*', None))  # for null reponses
-        elif '(' in hops and ')' in hops:
-            # parse ip
-            hop_ip = hops.split('(')[1].split(')')[0]
-            # parse latency
-            parts = hops.split()
-            for i, p in enumerate(parts):
-                if p.endswith('ms'):
-                    latency = float(parts[i - 1])
-                    results.append((hop_ip, latency))
-                    break
-    return results
+    logging.error(f'Traceroute failed\n        PING_TIMEOUT: {float(PING_TIMEOUT)*1000}')
+    return []
 
 def traceroute(host: str) -> List[Tuple[str, Optional[float]]]:
     """Runs and parses traceroute based on OS, returns list of hops and latency"""
-    host = _stantize_host(host)
+    host = _sanitize_host(host)
     logging.info(f"Running traceroute for: {host}")
     logging.info(f"OS Detected: {OS_NAME}")
     
-    if OS_NAME == 'Windows':
-        route_list = _run_traceroute_windows(host)
-    elif OS_NAME == 'Linux':
-        route_list = _run_traceroute_unix(host)
-    else:
-        raise NotImplementedError(f"Unsupported OS: {OS_NAME}")
+    route_list = _run_traceroute(host)
 
-    logging.info(f"Route list\n----------------------\n{route_list}\n----------------------")
+    logging.info(f"Route list\n\n{route_list}\n")
+
     return route_list
